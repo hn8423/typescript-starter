07249f68916726585d138a344c012577b436996d
hn8423 <joshep@pharma-bros.com>
2024-03-29 13:26:15 +0900

    모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  controllers: [],
+  providers: [ ],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { EmailModule } from 'src/email/email.module';
 
-@Module({})
+@Module({
+    imports:[EmailModule],
+    controllers:[UsersController],
+    providers:[UsersService,],
+})
 export class UsersModule {}
