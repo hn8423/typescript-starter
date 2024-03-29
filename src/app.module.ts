a8dd9449625f5812be798ee2b8d8a2ae9dee1e11
hn8423 <joshep@pharma-bros.com>
2024-03-29 15:18:39 +0900

    환경 변수 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
+console.log(`${__dirname}/config/env/.development.env`)
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+  ConfigModule.forRoot({
+    envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+    load: [emailConfig],
+    isGlobal:true,
+    validationSchema
+  })
+  ],
   controllers: [],
   providers: [ ],
 })
